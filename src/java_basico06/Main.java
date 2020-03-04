b47dcf991ad16a9a784c8fd04541b38d0a9b35df
Alisson <alissonhenrique98@gmail.com>
2020-03-04 20:52:07 -0300

    enviando a branch para o github

---
+++ src/java_basico06/Main.java
@@ -0,0 +1,17 @@
+package java_basico06;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Main app = new Main();
+		app.start();
+		
+
+	}
+
+	private void start() {
+		System.out.println("enviando esta branch pata o git hub");
+		
+	}
+
+}
