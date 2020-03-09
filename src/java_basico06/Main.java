787286f442b075c8ba128d4487a7f6c4d7311d56
Alisson <alissonhenrique98@gmail.com>
2020-03-09 19:37:59 -0300

    primeiro commmit

--- src/java_basico06/Main.java
+++ src/java_basico06/Main.java
@@ -5,13 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		Main app = new Main();
 		app.start();
-		
 
 	}
 
 	private void start() {
 		System.out.println("enviando esta branch pata o git hub");
-		
+
 	}
 
 }
